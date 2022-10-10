8f8af5b3fb5b65af81c5daa35fc70ec2906c6077
Alexander Nguyen <cernworks@gmail.com>
2022-10-10 17:49:34 -0400

    sql schemas and seeds

---
+++ server/db/schema/03_user_issues.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS userIssues CASCADE;
+
+CREATE TABLE user_issues (
+    user_id int NOT NULL,
+    issue_id int NOT NULL,
+    PRIMARY KEY (user_id, issue_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE,
+    FOREIGN KEY (issue_id) REFERENCES issues(id) ON UPDATE CASCADE
+)
---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name)
+VALUES
+('Name 1')
+('Name 2')
