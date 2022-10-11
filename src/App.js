422632376554d1ae8f2b652c59324e9c8a623ceb
Alexander Nguyen <cernworks@gmail.com>
2022-10-11 09:22:36 -0400

    creating axios provider

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
 import Issues from "./components/Issues"
 import NewIssues from './components/NewIssues';
-import axios from 'axios';
 
 function App() {
-  const [issues, setIssues] = useState([
-    {
-      id: 0,
-      issue_name: "Issue 1",
-      assigned_to: 'Name 1',
-      description: "Bug 1",
-      priority: "High"
-    },
-    {
-      id: 1,
-      issue_name: "Issue 2",
-      assigned_to: 'Name 3',
-      description: "Bug 2",
-      priority: "Low"
-    }
-  ])
-
-  useEffect(() => {
-    const url = 'http://localhost:4444/api/issues'
-    axios.get(url).then((res) => {
-      setIssues(res.data.response)
-    }).catch((e) => console.log(e))
-  }, [])
 
   return (
     <div className='d-flex' style={ {height:'100vh'} }>
-      <Issues width="30%" issues={issues}/>
+      <Issues width="30%"/>
       <NewIssues />
     </div>
   );
--- src/components/Issues.jsx
+++ src/components/Issues.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
 import { Accordion } from 'react-bootstrap'
+import axios from 'axios';
 
-export default function issues({ issues }) {
+export default function Issues() {
+    const [issues, setIssues] = useState([
+        {
+          id: 0,
+          issue_name: "Issue 1",
+          assigned_to: 'Name 1',
+          description: "Bug 1",
+          priority: "High"
+        },
+        {
+          id: 1,
+          issue_name: "Issue 2",
+          assigned_to: 'Name 3',
+          description: "Bug 2",
+          priority: "Low"
+        }
+      ])
+    
+      useEffect(() => {
+        const url = 'http://localhost:4444/api/issues'
+        axios.get(url).then((res) => {
+          setIssues(res.data.response)
+        }).catch((e) => console.log(e))
+      }, [])
+    
   return (
     <Accordion className='border-end 2px overflow-auto' defaultActiveKey="0" alwaysOpen style={{width: '30%'}}>
         {issues.map((issue, index) => (
